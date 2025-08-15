23040145423b8d2f80a28990b585e6f2c36d6ac6
JaimePrada11 <jandrespradato.11@gmail.com>
2025-08-15 03:05:08 -0500

    feat :sparkles: Creación de los clases utiles

---
+++ src/main/java/com/laura/paymentlinks/paymentlinks/util/JsonNodeConverter.java
@@ -0,0 +1,30 @@
+package com.laura.paymentlinks.paymentlinks.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class JsonNodeConverter implements AttributeConverter<JsonNode, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(JsonNode attribute) {
+        try {
+            return attribute != null ? objectMapper.writeValueAsString(attribute) : null;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error converting JsonNode to String", e);
+        }
+    }
+
+    @Override
+    public JsonNode convertToEntityAttribute(String dbData) {
+        try {
+            return dbData != null ? objectMapper.readTree(dbData) : null;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error converting String to JsonNode", e);
+        }
+    }
+}
