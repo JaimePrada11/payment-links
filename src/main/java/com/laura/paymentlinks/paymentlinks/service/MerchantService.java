39c49a8d64bdd838c60ee6aae9d2cfb58755d991
JaimePrada11 <jandrespradato.11@gmail.com>
2025-08-14 22:31:50 -0500

    feat :sparkles: Creacion del servicio y su logica del negocio de merchant

---
+++ src/main/java/com/laura/paymentlinks/paymentlinks/service/MerchantService.java
@@ -0,0 +1,32 @@
+package com.laura.paymentlinks.paymentlinks.service;
+
+import com.laura.paymentlinks.paymentlinks.dto.MerchantDto;
+import com.laura.paymentlinks.paymentlinks.model.Merchant;
+import com.laura.paymentlinks.paymentlinks.repository.MerchantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MerchantService {
+
+    private final MerchantRepository merchantRepository;
+
+    @Autowired
+    public MerchantService(MerchantRepository merchantRepository) {
+        this.merchantRepository = merchantRepository;
+    }
+
+
+    public MerchantDto findByEmail(String email) {
+        if (email == null) {
+            throw new NullPointerException("email is null");
+        }
+
+        return merchantRepository.findByEmail(email)
+                .map( merchant ->  new MerchantDto(
+                        merchant.getName(),
+                        merchant.getEmail()
+                ))
+                .orElse(null);
+    }
+}
